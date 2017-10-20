aace79f6e889cda2f98728d9a53df4a901a6afba
Parker Catalano <catalano.work@gmail.com>
2017-10-20 19:35:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence1)
   new_statement = []
   hi = sentence1.split
   hi.each do |words|
-    new_statement words.reverse
+    new_statement << words.reverse
   end
   new_statement.join
 end
