accdace860e2b04b08eb1796e21da82217fd055d
Parker Catalano <catalano.work@gmail.com>
2017-10-20 19:51:43 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence1)
-  new1 = []
-  save = sentence1.split
-  save.each do |words|
-    new1 << words.reverse
-  end
-  new1.join(" ")
-end
+# def reverse_each_word(sentence1)
+#   new1 = []
+#   save = sentence1.split
+#   save.each do |words|
+#     new1 << words.reverse
+#   end
+#   new1.join(" ")
+# end
 
 def reverse_each_word(sentence2)
   save2 = sentence2.split
